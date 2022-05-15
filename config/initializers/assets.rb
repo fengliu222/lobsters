ace97e31d30a5375996d3c3ee1bd1d018c4e094a
Docacola <444984@qq.com>
2022-05-15 18:58:44 +0000

    assets.precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '2.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( select2.js autosize.js jquery_class.js )
